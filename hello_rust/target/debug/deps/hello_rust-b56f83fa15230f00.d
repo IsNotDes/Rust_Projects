d152d5f1a1f689230b6d73b9bff1a040bd4e1c37
Enzo <enzog2106@gmail.com>
2025-05-08 16:05:31 +0200

    solved issue gitignore

--- hello_rust/target/debug/deps/hello_rust-b56f83fa15230f00.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Des\Documents\GitHub\Rust_Projects\hello_rust\target\debug\deps\hello_rust-b56f83fa15230f00.exe: src\main.rs
-
-C:\Users\Des\Documents\GitHub\Rust_Projects\hello_rust\target\debug\deps\hello_rust-b56f83fa15230f00.d: src\main.rs
-
-src\main.rs:
