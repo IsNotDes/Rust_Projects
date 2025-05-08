7cbcfe1394c2dbe5e039b8c8f3c09c99743eee48
Enzo <107392944+IsNotDes@users.noreply.github.com>
2025-05-08 15:54:46 +0200

    Delete hello_rust/target directory

--- hello_rust/target/debug/deps/hello_rust-c91ddf145e183191.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Des\Documents\GitHub\Rust_Projects\hello_rust\target\debug\deps\libhello_rust-c91ddf145e183191.rmeta: src\main.rs
-
-C:\Users\Des\Documents\GitHub\Rust_Projects\hello_rust\target\debug\deps\hello_rust-c91ddf145e183191.d: src\main.rs
-
-src\main.rs:
